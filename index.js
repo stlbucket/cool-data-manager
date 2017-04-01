3d8340ae058e467eb08d24049c862b937171dc16
stlbucket <stlbucket@gmail.com>
2017-03-31 19:03:41 -0500

    1.0.14

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./src');
+module.exports = {
+  CoolDataManager: require('./src/coolDataManager'),
+  CoolRelation: require('./src/coolRelation'),
+  CoolCollection: require('./src/coolCollection')
+};
